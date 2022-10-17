0b1f491c33d2f7872be3b1c19761d780315ed271
cagedeleph <10480524+cagedeleph@user.noreply.gitee.com>
2022-10-17 22:35:56 +0800

    2022年10月17日22:35:52

--- blackhorse/练习/STL/职工管理系统.cpp
+++ blackhorse/练习/STL/职工管理系统.cpp
@@ -36,44 +36,44 @@ int main()
     // worker = new Manager(2, "李四", 2);
     // worker->showInfo();
     //实例化管理者对象
-    //  WorkerManager wm;
+    WorkerManager wm;
 
-    // int choice = 0; // 用来存储用户的选项
+    int choice = 0; // 用来存储用户的选项
 
-    // while (true)
-    // {
-    // 	wm.Show_Menu();
+    while (true)
+    {
+        wm.Show_Menu();
 
-    // 	cout << "请输入您的选择: " << endl;
-    // 	cin >> choice;
+        cout << "请输入您的选择: " << endl;
+        cin >> choice;
 
-    // 	switch (choice)
-    // 	{
-    // 	case 0://退出系统
-    // 		wm.ExitSystem();
-    // 		break;
-    // 	case 1://增加职工
-    // 		break;
-    // 	case 2://显示职工
-    // 		break;
-    // 	case 3://删除职工
-    // 		break;
-    // 	case 4://修改职工
-    // 		break;
-    // 	case 5://查找职工
-    // 		break;
-    // 	case 6://排序职工
-    // 		break;
-    // 	case 7://清空文档
-    // 		break;
-    // 	default:
-    // 		system("cls"); //清屏
-    // 		break;
-    // 	}
-    // }
+        switch (choice)
+        {
+        case 0: //退出系统
+            wm.ExitSystem();
+            break;
+        case 1: //增加职工
+            break;
+        case 2: //显示职工
+            break;
+        case 3: //删除职工
+            break;
+        case 4: //修改职工
+            break;
+        case 5: //查找职工
+            break;
+        case 6: //排序职工
+            break;
+        case 7: //清空文档
+            break;
+        default:
+            system("cls"); //清屏
+            break;
+        }
+    }
 
-    // wm.Show_Menu();
+    wm.Show_Menu();
 
-    // system("pause");
-    // return 0;
+    system("pause");
+    return 0;
 }
